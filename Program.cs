cd5c3568e498f64551350c9ef61ecd54a314d656
daniel-s-souza <danielsantsouza94@gmail.com>
2023-06-14 10:48:02 -0300

    feat: implementa os testes no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjetoPOOSmartPhone.Models;
+
+Nokia nPhone = new Nokia("2799981-2530", "Nokia 5.3", "357278086773968", 128 );
+
+nPhone.Ligar();
+nPhone.ReceberLigacao();
+nPhone.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("2799981-2530", "Iphone X", "357278086773500", 128 );
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
