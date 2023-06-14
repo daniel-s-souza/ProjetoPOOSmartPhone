a3001d263f25805c431cde7dae32fa62d390c8c8
daniel-s-souza <danielsantsouza94@gmail.com>
2023-06-14 10:34:04 -0300

    feat: implementa classe nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,9 +5,16 @@ using System.Threading.Tasks;
 
 namespace ProjetoPOOSmartPhone.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+  // TODO: Herdar da classe "Smartphone"
+  public class Nokia : Smartphone
+  {
+    public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
     {
-         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
+
+    public override void InstalarAplicativo(string nomeApp)
+    {
+      Console.WriteLine($"Instalando {nomeApp} pela AppStore");
+    }
+  }
 }
